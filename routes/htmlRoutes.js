2134f332c5ed2090dbfa3c8f71e50eff1718009f
Amit Karmacharya <amitkarmacharya@Amits-MacBook-Pro.local>
2020-11-04 21:35:17 -0500

    created routes for /notes and /*

---
+++ routes/htmlRoutes.js
@@ -0,0 +1,16 @@
+const path = require('path');
+
+module.exports = function (app) {
+
+    // request to notes.html
+    app.get('/notes', (req,res) => {
+        res.sendFile(path.join(__dirname,'../public/notes.html'));
+    });
+
+    
+    // default url
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname,'../public/index.html'));
+    });
+    
+};
