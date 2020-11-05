55818ba1032b0a7caf80cb06339a92b4fabbeb6d
Amit Karmacharya <amitkarmacharya@Amits-MacBook-Pro.local>
2020-11-04 21:38:54 -0500

    added feature - get the list of notes from db.json

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -10,4 +10,21 @@ const DB_PATH = path.join(__dirname, "../db/db.json");
 
 module.exports = function (app) {
 
+    // response with the list of notes
+    app.get('/api/notes', async (req, res) => {
+
+        let notes = await readFileAsync(DB_PATH, "utf-8");
+
+        if (notes) {
+            notes = JSON.parse(notes);
+            res.json(notes);
+        } else {
+            res.send(false);
+        }
+
+        res.end();
+
+    });
+
+
 };
