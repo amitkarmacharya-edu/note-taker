ab8760dba0b627a03906bad8e7d6383ef4011f77
Amit Karmacharya <amitkarmacharya@Amits-MacBook-Pro.local>
2020-11-04 21:34:06 -0500

    create a server, server.js

---
+++ server.js
@@ -0,0 +1,30 @@
+// ==============================================================================
+// DEPENDENCIES
+// Series of npm packages that we will use to give our server useful functionality
+// ==============================================================================
+const express = require('express');
+
+const PORT = process.env.PORT || 3000;
+const HOST = "http://localhost:3000"; 
+
+const app = express();
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+// ================================================================================
+// ROUTER
+// The below points our server to a series of "route" files.
+// These routes give our server a "map" of how to respond when users visit or request data from various URLs.
+// ================================================================================
+
+
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+
+
+app.listen(PORT, (req, res) => {
+    console.log("The server has started listening in port " + PORT + " : " + HOST);
+});
